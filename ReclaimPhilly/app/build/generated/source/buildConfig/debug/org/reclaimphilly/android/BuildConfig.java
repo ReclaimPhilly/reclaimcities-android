3c6f5fef42f157a40d0ddb7823b135df9be1f87d
Ger O'Sullivan <gosu0001@synchronoss.com>
2017-01-27 13:43:30 -0500

    adding updated files

---
+++ ReclaimPhilly/app/build/generated/source/buildConfig/debug/org/reclaimphilly/android/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.reclaimphilly.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "org.reclaimphilly.reclaimphilly";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String PARSE_APPLICATION_ID = "eYOww0ZEGRtLUgVGJzAWvmOmgvwisq81c4TOHelB";
+  public static final String PARSE_CLIENT_KEY = "cElKQ8MalHETWbKcdElCuu6knq2W8UObXte9fDc0";
+  public static final String PARSE_REST_API_KEY = "GlMVP2ROEUTO6IGde6WE3evHmcBav0spMlIEfWFo";
+}
